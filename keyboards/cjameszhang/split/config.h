30fdca9df9973f7a2d0c8d0fe004c52523870584
James Zhang <cjameszhang@gmail.com>
2023-12-16 14:45:07 -0800

    update keymap

--- keyboards/cjameszhang/split/config.h
+++ keyboards/cjameszhang/split/config.h
@@ -1,4 +1,4 @@
-// Copyright 2023 James Zhang (@James Zhang)
+// Copyright 2023 James Zhang (@cjameszhang)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
--- keyboards/cjameszhang/split/keymaps/default/keymap.c
+++ keyboards/cjameszhang/split/keymaps/default/keymap.c
@@ -21,41 +21,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_BACKSLASH,
         KC_H, KC_J, KC_K, KC_L, KC_SEMICOLON, KC_QUOTE, KC_ENTER,
         KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_UP,
-        KC_RIGHT_CTRL, MO(3), KC_RIGHT_ALT, KC_RIGHT_GUI, KC_LEFT, KC_DOWN, KC_RIGHT
+        KC_RIGHT_CTRL, MO(2), TG(1), KC_RIGHT_GUI, KC_LEFT, KC_DOWN, KC_RIGHT
     ),
     [1] = LAYOUT_default(
-        KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4,
-        KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6,
-        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,
-        KC_SCROLL_LOCK, KC_A, KC_S, KC_D, KC_F, KC_G,
-        KC_LEFT_SHIFT, KC_Z, KC_X, KC_C, KC_V, KC_B,
-        KC_LEFT_CTRL, KC_LEFT_ALT, KC_SPACE, KC_HOME,
+        ___, ___, ___, ___, ___,
+        ___, ___, ___, ___, ___, ___, ___,
+        ___, ___, ___, ___, ___, ___,
+        KC_SCROLL_LOCK, ___, ___, ___, ___, ___,
+        ___, ___, ___, ___, ___, ___,
+        ___, ___, ___, KC_PAGE_UP,
 
-        KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
-        KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, KC_BACKSPACE,
-        KC_Y, KC_U, KC_UP, KC_O, KC_P, KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_BACKSLASH,
-        KC_H, KC_LEFT, KC_DOWN, KC_RIGHT, KC_SEMICOLON, KC_QUOTE, KC_ENTER,
-        KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_I,
-        KC_END, MO(3), KC_PAGE_UP, KC_PAGE_DOWN, KC_J, KC_K, KC_L
+        ___, ___, ___, ___, ___, ___, ___, ___,
+        ___, ___, ___, ___, ___, ___, ___,
+        ___, ___, KC_UP, ___, ___, ___, ___, ___,
+        ___, KC_LEFT, KC_DOWN, KC_RIGHT, ___, ___, ___,
+        ___, ___, ___, ___, ___, ___,
+        KC_PAGE_DOWN, ___, ___, ___, ___, ___, ___
     ),
     [2] = LAYOUT_default(
-        KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4,
-        KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6,
-        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,
-        KC_CAPS_LOCK, KC_A, KC_S, KC_D, KC_F, KC_G,
-        KC_LEFT_SHIFT, KC_Z, KC_X, KC_C, KC_V, KC_B,
-        KC_LEFT_CTRL, KC_LEFT_ALT, KC_SPACE, KC_LEFT_CTRL,
-
-        KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
-        KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, KC_BACKSPACE,
-        KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_BACKSLASH,
-        KC_H, KC_J, KC_K, KC_L, KC_SEMICOLON, KC_QUOTE, KC_ENTER,
-        KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_UP,
-        KC_RIGHT_CTRL, MO(3), KC_RIGHT_ALT, KC_RIGHT_GUI, KC_LEFT, KC_DOWN, KC_RIGHT
-    ),
-    [3] = LAYOUT_default(
         ___, ___, ___, ___, ___,
-        ___, DF(0), DF(1), DF(2), ___, ___, ___,
+        ___, ___, ___, ___, ___, ___, ___,
         ___, ___, ___, ___, ___, ___,
         ___, ___, ___, ___, ___, ___,
         ___, ___, ___, ___, ___, ___,
