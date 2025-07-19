b45b593ed6ba63d4fd2cd7010f15a6eb2e86bebf
James Zhang <cjameszhang@gmail.com>
2025-07-19 11:10:07 -0400

    update split mapping

--- keyboards/cjameszhang/split/config.h
+++ keyboards/cjameszhang/split/config.h
@@ -9,14 +9,23 @@
  */
 
 /* disable debug print */
-//#define NO_DEBUG
+// #define NO_DEBUG
 
 /* disable print */
-//#define NO_PRINT
+// #define NO_PRINT
 
 /* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
+// #define NO_ACTION_LAYER
+// #define NO_ACTION_TAPPING
+// #define NO_ACTION_ONESHOT
 
 #define SPLIT_HAND_PIN B6
+
+// Detecting which side is the master would be better, but can lead to this
+// issue, so let's just always use the left side:
+// https://github.com/qmk/qmk_firmware/issues/8990
+#define MASTER_LEFT
+
+// qmk bug: master_left doesn't really do anything
+// if always using one side to plug, just have one side use split detect, and the other side not
+// and make the other side very low timeout
--- keyboards/cjameszhang/split/keymaps/default/keymap.c
+++ keyboards/cjameszhang/split/keymaps/default/keymap.c
@@ -14,14 +14,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,
         LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G,
         KC_LEFT_SHIFT, KC_Z, KC_X, KC_C, KC_V, KC_B,
-        KC_LEFT_CTRL, KC_LEFT_ALT, KC_SPACE, KC_LEFT_CTRL,
+        KC_LEFT_CTRL, KC_LEFT_ALT, KC_SPACE, KC_ESC,
 
         KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
         KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, KC_BACKSPACE,
         KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_BACKSLASH,
         KC_H, KC_J, KC_K, KC_L, KC_SEMICOLON, KC_QUOTE, KC_ENTER,
         KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_UP,
-        KC_RIGHT_CTRL, MO(2), TG(1), KC_RIGHT_GUI, KC_LEFT, KC_DOWN, KC_RIGHT
+        KC_RIGHT_CTRL, MO(2), KC_RIGHT_GUI, KC_LEFT, KC_DOWN, KC_RIGHT
     ),
     [1] = LAYOUT_default(
         ___, ___, ___, ___, ___,
@@ -29,18 +29,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ___, ___, ___, ___, ___, ___,
         KC_SCROLL_LOCK, ___, ___, ___, ___, ___,
         ___, ___, ___, ___, ___, ___,
-        ___, ___, ___, KC_PAGE_UP,
+        ___, ___, ___, KC_I,
 
         ___, ___, ___, ___, ___, ___, ___, ___,
         ___, ___, ___, ___, ___, ___, ___,
         ___, ___, KC_UP, ___, ___, ___, ___, ___,
         ___, KC_LEFT, KC_DOWN, KC_RIGHT, ___, ___, ___,
         ___, ___, ___, ___, ___, ___,
-        KC_PAGE_DOWN, ___, ___, ___, ___, ___, ___
+        MO(3), MO(2), ___, ___, ___, ___
     ),
     [2] = LAYOUT_default(
         ___, ___, ___, ___, ___,
-        ___, ___, ___, ___, ___, ___, ___,
+        ___, DF(0), DF(1), ___, ___, ___, ___,
         ___, ___, ___, ___, ___, ___,
         ___, ___, ___, ___, ___, ___,
         ___, ___, ___, ___, ___, ___,
@@ -51,6 +51,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ___, KC_INSERT, KC_HOME, KC_PAGE_UP, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK, ___,
         ___, KC_DELETE, KC_END, KC_PAGE_DOWN, ___, ___, ___,
         ___, KC_AUDIO_MUTE, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, ___, ___,
-        ___, ___, ___, ___, ___, ___, ___
+        ___, ___, ___, ___, ___, ___
+    ),
+    [3] = LAYOUT_default(
+        ___, ___, ___, ___, ___,
+        ___, DF(0), DF(1), ___, ___, ___, ___,
+        ___, ___, ___, ___, ___, ___,
+        ___, ___, ___, ___, ___, ___,
+        ___, ___, ___, ___, ___, ___,
+        ___, ___, ___, ___,
+
+        ___, ___, ___, ___, ___, ___, ___, ___,
+        ___, KC_PRINT_SCREEN, KC_SCROLL_LOCK, KC_PAUSE, ___, ___, ___,
+        ___, KC_INSERT, KC_I, KC_PAGE_UP, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK, ___,
+        ___, KC_J, KC_K, KC_L, ___, ___, ___,
+        ___, KC_AUDIO_MUTE, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, ___, ___,
+        ___, ___, ___, ___, ___, ___
     ),
 };
